cc98b22fbf12ea94eec073ef4baf638f12f708f7
gzdx <gzdx1121@gmail.com>
2026-09-04 22:59:27 +0800

    chore(release): bump version to 3.0.1

--- client/app/build.gradle.kts
+++ client/app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.ldp.adskip"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
+        versionCode = 6
         versionName = "3.0"
     }
 
